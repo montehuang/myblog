8a35fcd8b2e083f1a86566bbc33de4c889d34e83
monte <865824187@qq.com>
2017-10-13 16:25:04 +0800

    add flask-migrate to migrate database

--- manage.py
+++ manage.py
@@ -2,14 +2,18 @@ import os
 from app import create_app, db
 from app.models import Role, User
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 @manager.shell
 def make_shell_context():
 	return dict(app=app, db=db, User=User, Role=Role)
+
+manager.add_command('db', MigrateCommand)
 	
 if __name__ == '__main__':
 	manager.run()
