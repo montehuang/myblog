3570068e99a23874e3e23eeb28545e42f65f583c
strongheart <865824187@qq.com>
2017-10-16 01:33:16 +0800

    add register module

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,30 @@
 from flask_wtf import Form 
+from ..models import User
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
 
+# 登陆表单
 class LoginForm(Form):
 	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
 	password = PasswordField('Password', validators=[Required()])
 	remember = BooleanField('Keep me logged in')
 	submit = SubmitField('Login')
+
+# 注册表单
+class RegisterForm(Form):
+	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+	username = StringField('Username', validators=[Required(), Length(1, 64), 
+		Regexp('^[A-za-z][A-za-z0-9_.]*$', 0, 'Username must have only letters, numbers, dots or underscores')])
+
+	password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
+	password2 = PasswordField('confirm password', validators=[Required()])
+	submit = SubmitField('Register')
+
+	def validate_email(self, field):
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('Email already registered.')
+
+	def validate_username(self, field):
+		if User.query.filter_by(username=field.data).first():
+			raise ValidationError('Username is in use.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,12 @@
 from flask import render_template, flash, redirect, request, url_for
 from flask_login import login_required, login_user, logout_user
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 from . import auth
+from .. import db
 
 
-@auth.route('/login', methods = ['GET', 'post'])
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
@@ -15,6 +16,18 @@ def login():
 			return redirect(request.args.get('next') or url_for('main.index'))
 		flash('Invalid username or password!')
 	return render_template('auth/login.html', form=form)
+
+@auth.route('/register', methods = ['GET', 'POST'])
+def register():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data,
+					username=form.username.data,
+					password=form.password.data)
+		db.session.add(user)
+		flash('You can login now.')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form=form)
 
 @auth.route('/secret')
 @login_required
