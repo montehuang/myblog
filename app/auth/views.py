73a8f14ef4ba94a60049720e5f109eaaa1ddfc06
strongheart <865824187@qq.com>
2017-10-17 01:55:40 +0800

    send a email to new users and let them confirm it

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, flash, redirect, request, url_for
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from ..models import User
 from .forms import LoginForm, RegisterForm
 from . import auth
 from .. import db
+from ..mail import send_mail
 
 
 @auth.route('/login', methods = ['GET', 'POST'])
@@ -25,9 +26,45 @@ def register():
 					username=form.username.data,
 					password=form.password.data)
 		db.session.add(user)
+		db.session.commit()
+		token = user.generate_confirmation_token()
+		send_mail(user.email, 'Confirm your account','auth/email/confirm', user = user, token = token)
+
 		flash('You can login now.')
 		return redirect(url_for('auth.login'))
 	return render_template('auth/register.html', form=form)
+@auth.route('/confirm/<token>', methods = ['GET', 'POST'])
+@login_required
+def confirm(token):
+	if current_user.confirmed:
+		return redirect(url_for('main.index'))
+	if current_user.confirm(token):
+		flash('You have confirmed your account.Thanks')
+	else:
+		flash('The confirmation link is invalid or has expired.')
+	return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+	if current_user.is_authenticated \
+		and not current_user.confirmed \
+		and request.endpoint[:5] != 'auth.' \
+		and request.endpoint != 'static':
+		return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous or current_user.confirmed:
+		return redirect(url_for('main.index'))
+	return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+	token = current_user.generate_confirmation_token()
+	send_mail(current_user.email, 'Confirm your Account', 'auth/email/confirm', user = current_user, token = token)
+	flash('A new confirmation email has been sent to you by email.')
+	return redirect(url_for('main.index'))
 
 @auth.route('/secret')
 @login_required
--- config.py
+++ config.py
@@ -6,7 +6,18 @@ class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guss'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+	MAIL_SERVER = 'smtp.sina.com'  # 邮件服务器地址
+	MAIL_PORT = 25               # 邮件服务器端口
+	MAIL_USE_TLS = True          # 启用 TLS
+	# MAIL_USE_SSL = True          # 启用 TLS
+	MAIL_USERNAME = os.environ.get('FLASKY_MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('FLASKY_MAIL_PASSWORD')
+	FLASKY_MAIL_SENDER = os.environ.get('FLASKY_MAIL_USERNAME')
+	FLASKY_MAIL_SUBJECT_PREFIX = 'monte blog'
+	# MAIL_USERNAME = "865824187@qq.com"
+	# MAIL_PASSWORD = "lecydmmxbsqtbbij"
+	MAIL_USERNAME = 'hqzmonte@sina.com'
+	MAIL_PASSWORD = 'hqz032340'
 	@staticmethod
 	def init_app(app):
 		pass
