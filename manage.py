6bdc92a7e80aeb3273bc73f01075492d74a79aaf
strongheart <865824187@qq.com>
2017-10-20 00:21:28 +0800

    seperate different requirement file and generate fake data using forgeryPy

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-from app.models import Role, User, Permission
+from app.models import Role, User, Permission, Post
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
@@ -11,7 +11,7 @@ migrate = Migrate(app, db)
 
 @manager.shell
 def make_shell_context():
-	return dict(app=app, db=db, User=User, Role=Role, Permission=Permission)
+	return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post = Post)
 
 manager.add_command('db', MigrateCommand)
 	
