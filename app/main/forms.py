ebcfcbb951b6a43bbe03bdb5942c2e0a886953e4
montehuang <hqzmonte@sina.com>
2018-03-01 03:07:57 +0800

    部分显示改成中文

--- app/main/forms.py
+++ app/main/forms.py
@@ -54,29 +54,29 @@ class TagListForm(Field):
 
 
 class NameForm(Form):
-	name = StringField('What is your name?', validators = [Required()])
-	submit = SubmitField('submit')
+	name = StringField('你的名字?', validators = [Required()])
+	submit = SubmitField('提交')
 
 class EditProfileForm(Form):
-	name = StringField('Real name', validators = [Length(0,64)])
-	location = StringField('Location', validators = [Length(0,64)])
-	about_me = TextAreaField('About me.')
-	submit = SubmitField('Submit')
+	name = StringField('真名', validators = [Length(0,64)])
+	location = StringField('位置', validators = [Length(0,64)])
+	about_me = TextAreaField('个人简介')
+	submit = SubmitField('提交')
 
 class NewEditProfileForm(Form):
 	all_info = PageDownField("博客内容", validators = [Required()])
 	submit = SubmitField("提交")
 
 class EditProfileAdminForm(Form):
-	email = StringField('Email', validators = (Required(), Length(1, 64), Email()))
-	username = StringField('Username', validators = (Required(), Length(1, 64), 
+	email = StringField('邮箱', validators = (Required(), Length(1, 64), Email()))
+	username = StringField('用户名', validators = (Required(), Length(1, 64), 
 		Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usename must have only letters, numbers, dots or underscores')))
-	confirmed = BooleanField('Confirmed')
-	role = SelectField('Role', coerce = int)
-	name = StringField('Real name', validators = [Length(0, 64)])
-	location = StringField('Location', validators = [Length(0, 64)])
-	about_me = TextAreaField('About me')
-	submit = SubmitField('Submit')
+	confirmed = BooleanField('确定')
+	role = SelectField('角色', coerce = int)
+	name = StringField('真名', validators = [Length(0, 64)])
+	location = StringField('位置', validators = [Length(0, 64)])
+	about_me = TextAreaField('个人简介')
+	submit = SubmitField('提交')
 
 	def __init__(self, user, *args, **kwargs):
 		super(EditProfileAdminForm, self).__init__(*args, **kwargs)
---
+++ md/about_me.md
@@ -0,0 +1,29 @@
+# 关于我
+
+----
+![](https://picabstract-preview-ftn.weiyun.com:8443/ftn_pic_abs_v2/368f83e38c9db8631db63c5b24166857c5fe39072dbadb4d350ede2367cad7a877510b2711e715b08bef802bcdba8fed?pictype=scale&from=30113&version=2.0.0.2&uin=865824187&fname=null5842779a518f02bb.jpg&size=240)
+
+我是黄全振，欢迎来到我的个人网站
+
+个人标签 `90后` `处女座`  `程序员` 
+
+爱好 `编码` `电影` `音乐`  `极限飞盘` `跑步` `户外`
+
+2014年毕业于淮阴工学院
+
+2014-2017先后在苏州，上海做过手游前段、后台管理网站的开发。
+
+目前使用**Flask**做公司部分后台的开发和游戏前端的开发
+
+使用和研究的技术有 *`Python`*  *`C++`*  *`Lua`*  *`flask`*  *`javascript`*  *`Cocos2dx`*  *`html`*  *`css`*
+
+*github*：[**montehuang**](https://github.com/montehuang)
+
+*邮箱*:  [*hqzmonte@sina.com*](mailto:hqzmonte@sina.com)
+
+*QQ*: 865824187
+
+-----
+# 关于本站
+
+前端使用*Bootstrap*，写博客使用*markdown*标记语言。后端使用*Python*的web应用框架*Flask*开发。本站会用来记录自己的学习、思考、观点等。谢谢您的关注！！
