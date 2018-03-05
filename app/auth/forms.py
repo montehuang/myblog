f6ec330b609e4310c6708992ca0b0419f991dd55
monte <hqzmonte@sina.com>
2018-03-05 11:22:31 +0800

    修改把英文改成中文

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,25 +6,25 @@ from wtforms import ValidationError
 
 # 登陆表单
 class LoginForm(Form):
-	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-	password = PasswordField('Password', validators=[Required()])
-	remember = BooleanField('Keep me logged in')
-	submit = SubmitField('Login')
+	email = StringField('邮件', validators=[Required(), Length(1, 64), Email()])
+	password = PasswordField('密码', validators=[Required()])
+	remember = BooleanField('保持登录')
+	submit = SubmitField('登录')
 
 # 注册表单
 class RegisterForm(Form):
-	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-	username = StringField('Username', validators=[Required(), Length(1, 64), 
+	email = StringField('邮箱', validators=[Required(), Length(1, 64), Email()])
+	username = StringField('用户名', validators=[Required(), Length(1, 64), 
 		Regexp('^[A-za-z][A-za-z0-9_.]*$', 0, 'Username must have only letters, numbers, dots or underscores')])
 
-	password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
-	password2 = PasswordField('confirm password', validators=[Required()])
-	submit = SubmitField('Register')
+	password = PasswordField('密码', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
+	password2 = PasswordField('确认密码', validators=[Required()])
+	submit = SubmitField('注册')
 
 	def validate_email(self, field):
 		if User.query.filter_by(email=field.data).first():
-			raise ValidationError('Email already registered.')
+			raise ValidationError('邮箱已经注册.')
 
 	def validate_username(self, field):
 		if User.query.filter_by(username=field.data).first():
-			raise ValidationError('Username is in use.')
+			raise ValidationError('用户名已存在.')
